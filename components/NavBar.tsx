b55cc491ecf8ab17b2371c1674bb3f2263607133
Waseem Asghar <wasghar@allivet.com>
2026-04-29 18:57:32 +0500

    first commit

---
+++ components/NavBar.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function NavBar(){
+    return(
+        <header>
+            <nav>
+                <Link href='/' className="logo">
+                    <Image src="/icons/logo.png" alt="logo" width={24} height={24}></Image>
+                    <p>DevEvents</p>
+                </Link>
+                <ul>
+                    <Link href='/'>Home</Link>
+                    <Link href='/'>Events</Link>
+                    <Link href='/'>Create Event</Link>
+                </ul>
+            </nav>
+        </header>
+    );
+}
