147e926d5c2dc0092fc3750674021bb96e9ceb04
Waseem Asghar <wasghar@allivet.com>
2026-04-30 17:28:38 +0500

    fixed image issue on explore button

--- components/ExploreBtn.tsx
+++ components/ExploreBtn.tsx
@@ -1,12 +1,12 @@
 'use client';
 
-import { Image } from "lucide-react";
+import Image from "next/image";
 
 export default function ExploreBtn() {
   return (
     <button type="button" id="explore-btn" className="mt-7 mx-auto" onClick={() => console.log('CLICKED')}>
       <a href="#events">Explore Events</a>
-      <img src="/icons/arrow-down.svg" alt="arrow-down" width={24} height={24}></img>
+      <Image src="/icons/arrow-down.svg" alt="arrow-down" width={24} height={24}></Image>
     </button>
   );
 }
